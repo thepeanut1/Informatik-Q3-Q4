5c8108b3b0f94f08b00da4c04c47f92a9c9b2cab
Pranit Vaidya <164780851+thepeanut1@users.noreply.github.com>
2024-12-05 08:48:36 +0100

    delKopf

---
+++ linkedList.py
@@ -0,0 +1,68 @@
+class Header:
+    def __init__(self):
+        self.length = 0
+        self.head = None
+        self.end = None
+        self.posInList = None
+
+    def addItem(self, value):
+        k = Node(value)
+        self.length += 1
+        
+        if self.length == 1:
+            self.head = k
+            self.posInList = k
+        else:
+            self.end.nextNode = k
+
+        self.end = k
+
+
+    def printList(self):
+        if self.length == 0:
+            print("List is empty")
+        else:
+            while True:
+                print(self.posInList.value)
+                if self.posInList == self.end:
+                    break
+                self.posInList = self.posInList.nextNode
+
+    def delKopf(self):
+        self.length -= 1
+        if self.head == self.end:
+            self.head = None
+        else:
+            self.head = self.head.nextNode
+
+        self.posInList = self.head
+
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.nextNode = None
+
+
+list1 = Header()
+list1.addItem(1)
+list1.addItem(3)
+list1.addItem(True)
+list1.addItem(78)
+list1.addItem(2)
+list1.addItem("test")
+
+
+
+
+list1.delKopf()
+list1.delKopf()
+list1.delKopf()
+list1.delKopf()
+list1.delKopf()
+
+
+
+list1.printList()
+    
