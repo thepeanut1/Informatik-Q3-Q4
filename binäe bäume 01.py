7ad2d22482544bf9866848aabd90062abec1af14
thepeanut1 <prnt445@outlook.com>
2025-01-15 18:02:26 +0100

    TREE VISUALISATION FINALLY WORKS!!! also got rid of all the prints + drawTree() +drawRight() +drawLeft() +makeList()

--- binäe bäume 01.py
+++ binäe bäume 01.py
@@ -1,4 +1,6 @@
-from Turtle import *
+from turtle import Turtle
+from random import randint
+
 
 class Binärbaum:
     def __init__(self):
@@ -10,34 +12,26 @@ class Binärbaum:
     def addItem(self, val, node = None, parent = None):
         self.current_counter +=1
         if parent == None:
-            print("Root was assigned to parent")
             parent = self.root
 
-            print("Node was created")
             node = Node(val)
 
         if self.root == None:
-            print("Root was created\n")
             self.root = node
             return 
 
         elif node.value > parent.value and parent.right == None:
             parent.right = node
-            print(f"New Node {node.value} was placed as the right child node of {parent.value}.")
 
         elif node.value <= parent.value and parent.left == None:
             parent.left = node
-            print(f"New Node {node.value} was placed as the left child node of {parent.value}.")
 
         elif node.value > parent.value:
-            print(f"Function was called again.\nOld parent: {parent.value}\nNew parent: {parent.right.value}")
             return self.addItem(val, node, parent.right)
         
         elif node.value <= parent.value:
-            print(f"Function was called again.\nOld parent: {parent.value}\nNew parent: {parent.left.value}")
             return self.addItem(val, node, parent.left)
         
-        print(f"{val} added \n \n")
         if self.current_counter > self.height:
             self.height = self.current_counter
 
@@ -55,7 +49,6 @@ class Binärbaum:
     def findItem(self, val, currNode = None):
         if currNode == None:
             currNode = self.root
-        print(currNode.value)
         if val == currNode.value:
             print("Value was found")
 
@@ -63,15 +56,80 @@ class Binärbaum:
             if currNode.right == None:
                 print(f"{val} not found")
             else:
-                print("Function was called again")
                 return self.findItem(val,currNode.right)
         
         elif val < currNode.value:
             if currNode.left == None:
                 print(f"{val} not found")
             else:
-                print("Function was called again")
                 return self.findItem(val,currNode.left)
+            
+    def drawTree(self):
+        if self.height > 7:
+            print("Tree hight over 7\nto many levels to print")
+            return
+        t = Turtle()
+        t.speed(0)
+        t.hideturtle()
+        t.screen.screensize(800,800)
+        t.penup()
+        t.pencolor("red")
+        t.goto(0,380)
+        t.write(self.root.value, align="center")
+
+        if self.root.right != None:
+            t.pendown()
+            t.goto((t.xcor() + 400/2**1), (t.ycor()-100))
+            self.drawRight(self.root.right,2, t.pos())
+
+        if self.root.left != None:
+            t.pendown()
+            t.goto(0,380)
+            t.goto((t.xcor() - 400/2**1), (t.ycor()-100))
+            self.drawLeft(self.root.left,2, t.pos())
+
+    def drawRight(self, node, depth, prevPos):
+        right = Turtle()
+        right.speed(0)
+        right.hideturtle()
+        right.penup()
+        right.pencolor("green")
+        right.goto(prevPos)
+        right.write(node.value, align="left")
+
+        if node.right != None:
+            right.pendown()
+            right.goto((right.xcor() + 400/(2**depth)), (right.ycor()-100))
+            self.drawRight(node.right, depth +1, right.pos())
+
+        if node.left != None:
+            right.penup()
+            right.goto(prevPos)
+            right.pendown()
+            right.goto((right.xcor() - 400/(2**depth)), (right.ycor()-100))
+            self.drawLeft(node.left, depth +1, right.pos())
+        
+    def drawLeft(self, node, depth, prevPos):
+        left = Turtle()
+        left.speed(0)
+        left.hideturtle()
+        left.penup()
+        left.pencolor("blue")
+        left.goto(prevPos)
+        left.write(node.value, align="right")
+
+        if node.right != None:
+            left.pendown()
+            left.goto((left.xcor() + 400/(2**depth)), (left.ycor()-100))
+            self.drawRight(node.right, depth +1, left.pos())
+
+        if node.left != None:
+            left.penup()
+            left.goto(prevPos)
+            left.pendown()
+            left.goto((left.xcor() - 400/(2**depth)), (left.ycor()-100))
+            self.drawLeft(node.left, depth +1, left.pos())
+
 """
 Search for a certain Num.:
 Try if num is bigger / smaller / same than root.
@@ -106,8 +164,19 @@ tree01.addItem(324)
 tree01.addItem(33)
 tree01.addItem(23)
 """
-list1 = [3,5,2,5,8,3,5,2,6,8,3,4,6,3,4,]
+list1 = [100,50,25,10,5,11,28,26,45,75,70,83,86,76,72,60,150,143,146,149,129,140,111,145,160,154,156,151,170,169,175]
+
+def makeList(length):
+    l = [500]
+    for i in range(length-1):
+        l.append(randint(100,999))
+    print(l)
+    return l
 
 tree01.listToTree(list1)
 
 tree01.findItem(6)
+
+tree01.drawTree()
+
+t = input("Press enter to close window:")
