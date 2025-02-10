855ab56cdfba1ecaf869b96a6417a68a6c85a21b
Pranit Vaidya <164780851+thepeanut1@users.noreply.github.com>
2025-02-10 09:31:10 +0100

    Add files via upload

---
+++ Uno/pygame test.py
@@ -0,0 +1,51 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+running = True
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+dt = 0
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    screen.fill("red")
+
+    pygame.draw.circle(screen, "green", player_pos,10)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    pygame.display.flip()
+    
+    dt = clock.tick(60) / 1000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#https://www.pygame.org/docs/
+#https://www.unorules.com/
