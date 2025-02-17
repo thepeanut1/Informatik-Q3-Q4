982bc0899bab8ca7301a59369b938cc75009b574
Sliimerr <152598487+Sliimerr@users.noreply.github.com>
2025-02-17 08:50:42 +0100

    updated players and giving cards to players

--- Uno/gameManager.py
+++ Uno/gameManager.py
@@ -1,59 +1,49 @@
 import random
+from player import *
 
 colors = ["Yellow", "Green", "Blue", "Red"]
 numbers = [1,2,4,5,6,7,8,9]
 
-class Cards:
-    def __init__(self, cardID, color, number):
-        self.cardID = cardID
-        self.color = color
-        self.number = number
 
-stack = []
+def start_game():
+    class Cards:
+        def __init__(self, cardID, color, number):
+            self.cardID = cardID
+            self.color = color
+            self.number = number
 
-for color in colors:
-    for num in numbers:
-        card = Cards((color,num), color, num)
-        stack.append(card)
+    stack = []
 
-
-random.shuffle(stack)
-print(stack[0].cardID)
-print("\n")
-for i in range(5):
-    print(stack[i].cardID)
-
-class Player:
-    def __init__(self, ID, name):
-        self.ID = ID
-        self.name = name
-        self.deck = []
+    for color in colors:
+        for num in numbers:
+            card = Cards((color,num), color, num)
+            stack.append(card)
 
 
-    def check_cards(self):
-        pass
+    random.shuffle(stack)
+    #print(stack[0].cardID)
+    print("\n")
+    for i in range(5):
+        print(stack[i].cardID)
 
-    def hide_cards(self):
-        pass
-    def show_cards(self):
-        pass
-
-    def give_cards(self):
-        for i in range(5):
-            self.deck.append(stack[i])
-        for i in range(5):
-            stack.pop(i)
+    playeramount = int(input("Please enter the number of players:"))
+    playerlist = []
 
 
-p1 = Player(1, "Bean")
-p1.give_cards()
+    for i in range(playeramount):
+        player = Player(i+1, input(f"Player {i+1}, please enter a name:"))
+        playerlist.append(player)
+        player.give_cards()
 
-p2 = Player(2, "Peanut")
-p2.give_cards()
 
-print("\n")
-for i in range(5):
-    print(stack[i].cardID)
-print("\n")
-for i in range(5):
-    print(p1.deck[i].cardID)
+    print("\n")
+    for i in range(5):
+        print(playerlist[0].deck[i].cardID)
+    print("\n")
+    for i in range(5):
+        print(playerlist[1].deck[i].cardID)
+
+    
+
+
+start_game()
