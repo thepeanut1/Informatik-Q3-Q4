4f4ea013f279442ada5723a7bc6eede5a104ca72
Pranit Vaidya <164780851+thepeanut1@users.noreply.github.com>
2025-02-17 09:35:54 +0100

    3 player

--- Uno/pygame test.py
+++ Uno/pygame test.py
@@ -9,10 +9,10 @@ running = True
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 dt = 0
 
-bg = pygame.image.load("Uno/bg.png")
+bg = pygame.image.load("bg.png")
 
-
-
+playerID = 0
+myCard = gameManager.playerlist[playerID].deck
 
 while running:
     # poll for events
@@ -25,18 +25,27 @@ while running:
 
     screen.blit(bg,[0,0])
 
-    for i in range(len(gameManager.p1.deck)):
-        spacing = 400 / len(gameManager.p1.deck)
-        screen.blit(pygame.transform.scale(pygame.image.load("Uno/cards/"+gameManager.p1.deck[i].color+"_"+str(gameManager.p1.deck[i].number)+".jpg"),[150,150]),[spacing * i + 400, 700])
+    for i in range(len(myCard)):
+        spacing = 400 / len(myCard)
+        screen.blit(pygame.transform.scale(pygame.image.load("cards/"+myCard[i].color+"_"+str(myCard[i].number)+".jpg"),[150,150]),[spacing * i + 400, 700])
 
 
-
-    for i in range(len(gameManager.p2.deck)):
-        spacing = 300 / len(gameManager.p2.deck)
-        screen.blit(pygame.transform.scale(pygame.image.load("Uno/cards/back.png"),[150,150]),[spacing * i + 425, 100])
-
+    if len(gameManager.playerlist) == 2:
+        for i in range(len(gameManager.playerlist[1].deck)):
+            spacing = 300 / len(gameManager.playerlist[1].deck)
+            screen.blit(pygame.transform.scale(pygame.image.load("cards/back.png"),[150,150]),[spacing * i + 425, 100])
+        
+    if len(gameManager.playerlist) == 3:
+        for i in range(len(gameManager.playerlist[playerID + 2].deck)):
+            spacing = 300 / len(gameManager.playerlist[playerID + 2].deck)
+            screen.blit(pygame.transform.scale(pygame.image.load("cards/back.png"),[150,150]),[spacing * i + 425, 100])
+ 
+        for i in range(len(gameManager.playerlist[playerID + 1].deck)):
+            spacing = 300 / len(gameManager.playerlist[playerID + 1].deck)
+            screen.blit(pygame.transform.scale(pygame.image.load("cards/back.png"),[150,150]),[100, spacing * i + 200])
 
     pygame.display.flip()
+
     
     dt = clock.tick(60) / 1000
 
@@ -52,6 +61,7 @@ while running:
 
 
 
+#py -m pip install -U pygame --user
 
 #https://www.pygame.org/docs/
 #https://www.unorules.com/
